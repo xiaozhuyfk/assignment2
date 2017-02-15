073ff6bc77de99654475c3d2ff9ad6cbdf49908a
xiaozhuyfk <xiaozhuyfk@gmail.com>
2017-02-15 15:50:24 -0500

    saxpy

--- saxpy/saxpy.cu
+++ saxpy/saxpy.cu
@@ -88,7 +88,7 @@ void saxpyCuda(int N, float alpha, float* xarray, float* yarray, float* resultar
     double overallDuration = endTime - startTime;
     double kernelDuration = kernelEndTime - kernelStartTime;
     printf("Overall time: %.3f ms\t\t[%.3f GB/s]\n", 1000.f * overallDuration, toBW(totalBytes, overallDuration));
-    printf("Kernel time: %.3f ms\n", 1000.f * kernelDuration);
+    printf("Overall time: %.3f ms\t\t[%.3f GB/s]\n", 1000.f * kernelDuration, toBW(totalBytes, kernelDuration));
 
     //
     // TODO free memory buffers on the GPU
